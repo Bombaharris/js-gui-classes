a51124979fc481bdca70c5446dea2e09adccc934
Rafał Zielonka <bombaharris@gmail.com>
2012-06-29 13:42:24 +0200

    license added

--- menuHarmony.js
+++ menuHarmony.js
@@ -1,3 +1,24 @@
+/*  
+    MenuHarnony js it's a part of js-gui-classes Prototype JavaScript Framework based classes.
+    http://github.com/Bombaharris/js-gui-classes
+    Rafał Zielonka
+    Varsion 1.0 (2012-06-19)
+
+    Copyright (C) 2012  Rafał Zielonka
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/> 
+ */
 var Harmony = Class.create({
     initialize: function(menuId, options) {
         this.menu = $(menuId);
